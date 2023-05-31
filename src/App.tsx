771c727b2e9f1d0303602836daec20f8fedd443d
aliev008 <hfe.as@yandex.ru>
2023-06-01 02:15:15 +0300

    feat: added sound playing by click

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import sounds from './sounds.json';
 
-function App() {
+const App = () => {
+
+  const [audioButtons, setAudioButtons] = useState([]);
+
+  // Fetching audio buttons from assets
+
+  // useEffect(() => {
+  //   const fetchSounds = async () => {
+  //     try {
+  //       const response = await fetch("sounds.json");
+  //       const data = await response.json();
+  //       console.log({ data });
+  //       setAudioButtons(data);
+
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+
+  //   };
+
+  //   fetchSounds();
+  // }, []);
+  
+  const buttons = Object.keys(sounds);
+
+  function play(event: any) {
+    const elem = event.target;
+    const child = elem.querySelector("audio");
+    child.play();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='drum-machine'>
+        <div className='left'>
+          {buttons.map((elem, index) => {
+            return (<div key={index} className='drum-pad' onClick={(event) => play(event)}>{elem} <audio src={sounds[elem as keyof typeof sounds].src}></audio></div>);
+          })}
+        </div>
+        <div className='right'>
+          <div id='display'></div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
